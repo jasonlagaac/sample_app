6a7f60ea3d216471c855e90445b44b1673fc7193
Jason Lagaac <jason.lagaac@me.com>
2011-09-22 11:50:02 +0800

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -52,4 +52,52 @@ describe UsersController do
       response.should have_selector("title", :content => "Sign up")
     end
   end
+
+  describe "POST 'create'" do
+    describe "failure" do
+      before(:each) do
+        @attr = { :name => "", :email => "", :password => "", :password_confirmation => "" }
+      end
+
+      it "should not create a user" do 
+        lambda do
+          post :create, :user => @attr
+        end.should_not change(User, :count)
+      end
+
+
+      it "should have the right title" do
+        post :create, :user => @attr
+        response.should have_selector("title", :content => "Sign up")
+      end
+
+      it "should render the 'new' page" do
+        post :create, :user => @attr
+        response.should render_template('new')
+      end
+    end
+
+    describe "success" do
+      before(:each) do
+        @attr = { :name => "Harvey Birdman", :email => "harvey.birdman@example.com", 
+                  :password => "ill take the case", :password_confirmation => "ill take the case" }
+      end
+
+      it "should create a user" do 
+        lambda do
+          post :create, :user => @attr
+        end.should change(User, :count).by(1)
+      end
+      
+      it "should redirect to the user show page" do
+        post :create, :user => @attr
+        response.should redirect_to(user_path(assigns(:user)))
+      end
+      
+      it "should have a welcome message" do
+        post :create, :user => @attr
+        flash[:success].should =~ /welcome to the sample app/i
+      end
+    end
+  end
 end
